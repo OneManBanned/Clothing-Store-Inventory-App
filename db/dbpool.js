78967b164bbe9de1ba868f99a81af2b0e000a44c
Brendan <brendanlawless@hotmail.com>
2024-11-14 16:41:13 +0000

    refactor: make connection string conditional

--- db/dbpool.js
+++ db/dbpool.js
@@ -2,8 +2,12 @@ import pkg from "pg";
 
 const { Pool } = pkg;
 
+const currentConnectionString = process.env.STAGE === 'development' 
+    ? `postgresql://${process.env.USER}:${process.env.DATABASE_PASSWORD}@localhost:5432/kino_leather`
+    : `postgresql://postgres.hlvbjqdwrqfcdzbksmdg:${process.env.KINO_LEATHER_SUPABASE_PASSWORD}@aws-0-eu-west-2.pooler.supabase.com:6543/postgres`
+
 const pool = new Pool({
-    connectionString: `postgresql://${process.env.USER}:${process.env.DATABASE_PASSWORD}@localhost:5432/kino_leather`
+    connectionString: currentConnectionString
 })
- 
+
 export default pool;
