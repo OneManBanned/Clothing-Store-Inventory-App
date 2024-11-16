4da88b1be2ecaaf80303d1c5e9c940e729af9546
Brendan <brendanlawless@hotmail.com>
2024-11-16 19:07:41 +0000

    feat: update item

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,32 +1,82 @@
-import { queryCreateItem, queryCategory, queryItems } from "../db/quieres.js";
+import {
+  queryPostUpdateItem,
+  queryCreateItem,
+  queryItem,
+  queryCategory,
+  queryItems,
+} from "../db/quieres.js";
 import { body, validationResult } from "express-validator";
 
 const validateData = [
-    body("name")
-        .trim()
-        .notEmpty()
-        .isAlpha()
-        .withMessage("This is meant to be Alphanumeric characters only"),
+  body("name")
+    .trim()
+    .notEmpty()
+    .isAlpha()
+    .withMessage("This is meant to be Alphanumeric characters only"),
+  body("color")
+    .trim()
+    .notEmpty()
+    .isAlpha()
+    .withMessage("This is meant to be Alphanumeric characters only"),
+  body("size")
+    .trim()
+    .notEmpty()
+    .isInt()
+    .withMessage("This is meant to be a number"),
+  body("price").trim().isDecimal().withMessage("price is meant to be decimal"),
+  body("category").notEmpty().withMessage("enter a category"),
 ];
 
 export async function getItems(req, res) {
-    const { rows: items } = await queryItems();
-    const { rows: categories } = await queryCategory();
-    res.render("items", { items: items, categories: categories });
+  const { rows: items } = await queryItems();
+  const { rows: categories } = await queryCategory();
+  res.render("items", { items: items, categories: categories });
 }
 
 export const postItems = [
-    validateData,
-    async (req, res, next) => {
-        const { name, color, size, price, category } = req.body;
+  validateData,
+  async (req, res) => {
+    const { name, color, size, price, category } = req.body;
 
-        const errors = validationResult(req);
+    const errors = validationResult(req);
 
-        if (!errors.isEmpty()) {
-            res.status(400).render("index", { errors: errors.array() });
-        } else {
-        await queryCreateItem(name, color, size, price, category);
-        res.redirect("/");
-        }
-    },
+    if (!errors.isEmpty()) {
+      res.status(400).render("index", { errors: errors.array() });
+    } else {
+      await queryCreateItem(name, color, size, price, category);
+      res.redirect("/");
+    }
+  },
+];
+
+export async function updateItem(req, res) {
+  const { id } = req.params;
+  const { rows: item } = await queryItem(id);
+  const { rows: categories } = await queryCategory();
+  const { name, color, size, price, type, item_id } = item[0];
+  res.render("editItem", {
+    name: name,
+    color: color,
+    size: size,
+    price: price,
+    type: type,
+    categories: categories,
+    item_id: item_id,
+  });
+}
+
+export const postUpdateItem = [
+  validateData,
+  async (req, res) => {
+    const { name, color, size, price, category } = req.body;
+    const { id: item_id } = req.params;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).render("index", { errors: errors.array() });
+    } else {
+      await queryPostUpdateItem(item_id, name, color, size, price, category);
+      res.redirect("/");
+    }
+  },
 ];
--- db/quieres.js
+++ db/quieres.js
@@ -1,19 +1,32 @@
 import pool from "./dbpool.js";
 
+// item quieres
+
 export const queryItems = async () => {
-    const items = await pool.query('SELECT * FROM item INNER JOIN category ON item.type = category.id;')
+    const items = await pool.query('SELECT item_id, name, color, size, price, type FROM item INNER JOIN category ON type_id = category_id;')
     return items;
 }
+
+export const queryItem = async (id) => {
+    const item = await pool.query('SELECT item_id, name, color, size, price, type FROM item INNER JOIN category ON type_id = category_id WHERE item_id = ($1)', [id])
+    return item;
+}
+
+export const queryCreateItem = async(name, color, size, price, category) => {
+    await pool.query('INSERT INTO item (name, color, size, price, type_id) VALUES ($1, $2, $3, $4, $5)', [name, color, size, price, category])
+}
+
+export const queryPostUpdateItem = async( item_id, name, color, size, price, cateogry ) => {
+    await pool.query('UPDATE item SET name = ($2), color = ($3), size = ($4), price = ($5), type_id = ($6) WHERE item_id = ($1);', [ item_id, name, color, size, price, cateogry ])
+}
+
+//category quieres
 
 export const queryCategory = async () => {
     const categories = await pool.query('SELECT * FROM category')
     return categories;
 }
 
-export const queryCreateItem = async(name, color, size, price, category) => {
-    await pool.query('INSERT INTO item (name, color, size, price, type) VALUES ($1, $2, $3, $4, $5)', [name, color, size, price, category])
-}
-
 export const queryCreateCategory = async(type) => {
-    await pool.query('INSERT INTO category (type) VALUES ($1)', [type])
+    await pool.query('INSERT INTO category (type_id) VALUES ($1)', [type])
 }
